3c0cf4d80ca6d7224eee116272fb60e9de2ed827
Pedro結太 <cesar_pedro@outlook.com>
2024-06-23 13:49:46 -0300

    update import script

--- import-images.js
+++ import-images.js
@@ -1,60 +1,82 @@
-import fs from 'fs'
-import path from 'path'
-import axios from 'axios'
-import { fileURLToPath } from 'url'
+import fs from "fs";
+import path from "path";
+import axios from "axios";
+import { fileURLToPath } from "url";
 
-// Directory containing the folders with images
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const directoryPath = path.join(__dirname, 'celebrity_faces')
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const directoryPath = path.join(__dirname, "celebrity_faces");
 
-// Function to convert image to base64
 const imageToBase64 = (filePath) => {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, (err, data) => {
-      if (err) reject(err)
-      resolve(data.toString('base64'))
-    })
-  })
-}
+      if (err) reject(err);
+      if (data) resolve(data.toString("base64"));
+    });
+  });
+};
 
-// Function to send HTTP POST request
 const sendPostRequest = async (base64Image) => {
   try {
-    const response = await axios.post('http://localhost:3000/user/create', {
-      base64Image
-    })
-    console.log('Response:', response.data)
+    const response = await axios.post("http://localhost:3000/user/create", {
+      base64Image,
+    });
+    console.log("Response:", response.data);
   } catch (error) {
-    console.error('Error:', error.message)
+    console.error("Error:", error.message);
   }
-}
+};
 
-// Loop through all folders and files
-fs.readdir(directoryPath, (err, folders) => {
-  if (err) {
-    return console.error('Unable to scan directory:', err.message)
+const processFile = async (filePath) => {
+  try {
+    const base64Image = await imageToBase64(filePath);
+    await sendPostRequest(base64Image);
+  } catch (error) {
+    console.error("Error processing file:", filePath, error.message);
   }
+};
 
-  folders.forEach((folder) => {
-    const folderPath = path.join(directoryPath, folder)
+const processQueue = async (queue, concurrency = 5) => {
+  const running = [];
+  while (queue.length > 0 || running.length > 0) {
+    while (running.length < concurrency && queue.length > 0) {
+      const filePath = queue.shift();
+      const promise = processFile(filePath).then(() => {
+        running.splice(running.indexOf(promise), 1);
+      });
+      running.push(promise);
+    }
+    await Promise.race(running);
+  }
+};
 
+const processFolder = async (folderPath) => {
+  return new Promise((resolve, reject) => {
     fs.readdir(folderPath, (err, files) => {
       if (err) {
-        return console.error('Unable to scan folder:', err.message)
+        reject(err);
+        return;
       }
+      const queue = files
+        .filter((file) => file !== ".DS_Store")
+        .map((file) => path.join(folderPath, file));
+      processQueue(queue).then(resolve).catch(reject);
+    });
+  });
+};
 
-      files.forEach(async (file) => {
-        if (file === '.DS_Store') return // Ignore .DS_Store files
+fs.readdir(directoryPath, async (err, folders) => {
+  if (err) {
+    console.error("Unable to scan directory:", err.message);
+    return;
+  }
 
-        const filePath = path.join(folderPath, file)
-        try {
-          const base64Image = await imageToBase64(filePath)
-          await sendPostRequest(base64Image)
-        } catch (error) {
-          console.error('Error processing file:', file, error.message)
-        }
-      })
-    })
-  })
-})
+  for (const folder of folders) {
+    const folderPath = path.join(directoryPath, folder);
+    try {
+      await processFolder(folderPath);
+    } catch (error) {
+      console.error("Error processing folder:", folder, error.message);
+    }
+  }
+});
